9dbd9d7827dcad7bb6251a0437998953d9333f6a
dipadana <putudipadana@gmail.com>
2019-09-11 10:55:53 +0700

    Release 0

--- todo.js
+++ todo.js
@@ -1,1 +1,44 @@
+const toDoControllers = require('./Controllers/toDoControllers.js')
 
+const command = process.argv[2];
+const arguments = process.argv.slice(3);
+
+switch(command){
+  case 'list':
+    console.log('Ini daftar list');
+    break;
+  case 'add':
+    console.log('data sudah ditambahkan');
+    break;
+  case 'findById':
+    console.log('cari berdasarkan id');
+    break;
+  case 'delete':
+    console.log('data di hapus');
+    break;
+  case 'complete':
+    console.log('komplit');
+    break;
+  case 'uncomplete':
+    console.log('belum komplit');
+    break;
+  case 'help':
+    console.log('$ node todo.js');
+    console.log('$ node todo.js help');
+    console.log('$ node todo.js list');
+    console.log('$ node todo.js add <task_content>');
+    console.log('$ node todo.js findById <task_id>');
+    console.log('$ node todo.js delete <task_id>');
+    console.log('$ node todo.js complete <task_id>');
+    console.log('$ node todo.js uncomplete <task_id>');
+    break;
+  default:
+    console.log('$ node todo.js');
+    console.log('$ node todo.js help');
+    console.log('$ node todo.js list');
+    console.log('$ node todo.js add <task_content>');
+    console.log('$ node todo.js findById <task_id>');
+    console.log('$ node todo.js delete <task_id>');
+    console.log('$ node todo.js complete <task_id>');
+    console.log('$ node todo.js uncomplete <task_id>');
+}
