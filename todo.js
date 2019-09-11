dac16b884b90b672b201ecce7062ce7116b1ddba
Satyo Wicaksana <satyowicaksana@gmail.com>
2019-09-11 10:41:23 +0700

    release 0

--- todo.js
+++ todo.js
@@ -1,1 +1,18 @@
 
+let command = process.argv[2];
+let parameters = process.argv.slice(3);
+
+switch(command) {
+    case 'list':
+        console.log('this is list');
+    break;
+    default:
+        console.log('node todo.js');
+        console.log('node todo.js help');
+        console.log('node todo.js list');
+        console.log('node todo.js add <task_content>');
+        console.log('node todo.js findById <task_id>');
+        console.log('node todo.js delete <task_id>');
+        console.log('node todo.js complete <task_id>');
+        console.log('node todo.js uncomplete <task_id>');
+}
